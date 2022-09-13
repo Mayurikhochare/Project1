a38cee0ee3f607d1b2c5dfaa9a46bae54ad08fe4
Khochare.Mayuri <mayurikhochare23@gmail.com>
2022-09-13 16:34:42 +0530

    commit

---
+++ src/Leap.java
@@ -0,0 +1,14 @@
+public class Leap {
+    public static void main(String[] args) {
+        int year =2020;
+        boolean leap=false;
+        if ((year %4==0) && (year%100!=0) || (year %400==0)){
+            System.out.println(year+"is a leap year");
+        }
+       else{
+            System.out.println(year+"not leap year");
+
+        }
+    }
+
+}
